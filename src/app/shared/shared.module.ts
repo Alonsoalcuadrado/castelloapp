4bb9adc78fd358d5827cf28e98c2d26447756167
Erick Alonso <alcuadradoalonso@gmail.com>
2023-01-10 16:38:08 -0600

    main button cta created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { InfoPanelComponent } from './info-panel/info-panel.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    InfoPanelComponent,
+    ButtonComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
+    InfoPanelComponent,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
